a19309995740126bfdf0ebbdda284a32394ed9f2
Teemu <teemu.on.ihminen@gmail.com>
2022-11-20 21:07:22 +0200

    Move files

--- sqlsugar/__init__.py
+++ sqlsugar/__init__.py
@@ -4,20 +4,13 @@ from enum import Enum, auto
 from alembic.autogenerate import produce_migrations
 from alembic.migration import MigrationContext
 from alembic.operations import Operations
-from alembic.operations.ops import (
-    AddColumnOp,
-    CreateIndexOp,
-    CreateTableOp,
-    ModifyTableOps,
-)
 from sqlalchemy import MetaData
 from sqlalchemy.engine import Connection
 
+from .exceptions import UnsupportedOperation
+from .operations import DiffToOperation
+
 logger = logging.getLogger(__name__)
-
-
-class UnsupportedOperation(Exception):
-    pass
 
 
 class MigrationErrorType(Enum):
@@ -36,8 +29,6 @@ def migrate(
     mc = MigrationContext.configure(connection)
     migrations = produce_migrations(mc, metadata)
     upgrade_ops = migrations.upgrade_ops.ops
-    # print(produce_migrations(mc, metadata).upgrade_ops)
-    # diff = compare_metadata(mc, metadata)
 
     context = MigrationContext.configure(connection)
     operations = Operations(context)
@@ -80,39 +71,3 @@ def migrate(
         logger.info("%i operations", len(upgrade_ops))
 
     return None
-
-
-class DiffToOperation:
-    def __init__(self, operations: Operations) -> None:
-        self.op = operations
-
-    def CreateTableOp(self, op: CreateTableOp) -> None:
-        logger.info("%s Creating a table: %r", op.table_name, op)
-        op.to_table().create(bind=self.op.migration_context.connection)
-
-    def AddColumnOp(self, op: AddColumnOp) -> None:
-        logger.info("%s: Adding a column to %r", op.table_name, op)
-        self.op.add_column(op.table_name, op.column)
-
-    def ModifyTableOps(self, op: ModifyTableOps) -> None:
-        logger.info("%s: Modifying a table", op.table_name)
-        for op2 in op.ops:
-            name = op2.__class__.__name__
-            if hasattr(self, name):
-                getattr(self, name)(op2)
-            else:
-                table_name = getattr(op2, "table_name", "")
-                logger.warn(
-                    "%s: Unsupported operation for %r",
-                    table_name,
-                    op2.__class__.__name__,
-                )
-
-    def CreateIndexOp(self, op: CreateIndexOp) -> None:
-        logger.info("Creating an index: %s %r", op.table_name, op)
-        self.op.create_index(
-            op.index_name, op.table_name, [x.name for x in op.to_index().expressions]
-        )
-
-
-# ModifyTableOps
---
+++ sqlsugar/operations.py
@@ -0,0 +1,44 @@
+import logging
+
+from alembic.operations import Operations
+from alembic.operations.ops import (
+    AddColumnOp,
+    CreateIndexOp,
+    CreateTableOp,
+    ModifyTableOps,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class DiffToOperation:
+    def __init__(self, operations: Operations) -> None:
+        self.op = operations
+
+    def CreateTableOp(self, op: CreateTableOp) -> None:
+        logger.info("%s Creating a table: %r", op.table_name, op)
+        op.to_table().create(bind=self.op.migration_context.connection)
+
+    def AddColumnOp(self, op: AddColumnOp) -> None:
+        logger.info("%s: Adding a column to %r", op.table_name, op)
+        self.op.add_column(op.table_name, op.column)
+
+    def ModifyTableOps(self, op: ModifyTableOps) -> None:
+        logger.info("%s: Modifying a table", op.table_name)
+        for op2 in op.ops:
+            name = op2.__class__.__name__
+            if hasattr(self, name):
+                getattr(self, name)(op2)
+            else:
+                table_name = getattr(op2, "table_name", "")
+                logger.warn(
+                    "%s: Unsupported operation for %r",
+                    table_name,
+                    op2.__class__.__name__,
+                )
+
+    def CreateIndexOp(self, op: CreateIndexOp) -> None:
+        logger.info("Creating an index: %s %r", op.table_name, op)
+        self.op.create_index(
+            op.index_name, op.table_name, [x.name for x in op.to_index().expressions]
+        )
